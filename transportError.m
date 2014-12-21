fb52384c689d0d18997daac555b61294a12cc152
v0lta <moritzwolter@gmx.de>
2014-12-21 23:51:44 +0100

    "Getting there. Missing only the error analysis for the heat and wave equation."

---
+++ transportError.m
@@ -0,0 +1,46 @@
+%compute the error of the 2d heat equation in 2d.
+
+mu = 0.6;
+%mu = dx/dt;
+
+tend = 0.5;
+J = 30;
+
+%x in [0,1].
+dx = 1/J;
+%y in [0,1];
+dy = 1/J;
+% mu = dt/dx^2;
+dt = mu*dx
+
+steps = floor(tend/dt)
+
+[x,y] = meshgrid(linspace(0,1,J));
+
+%the boundary conditions are zero (of homogeneous diriclet type).
+%the initial solution is u0(x,y) = sin(pi x) sin(pi y).
+U = sin(pi*x).*sin(pi*y); 
+U1 = zeros(J);
+U2 = zeros(J);
+%time loop
+for t = 1:steps
+    elements = 2:J-1;  
+    for i = 1:1:J
+        %compute the columns where x is const.
+        U1(elements,i) = mu*U(elements+1,i); 
+        %compute the columns where y is const.
+        U2(i,elements) = mu*U(i,elements+1);
+    end
+    Unew = (1 - 2*mu) .* U + U1 + U2;
+    U = Unew;
+    %surf(x,y,U); axis([0 1 0 1 -1 1 -1 1]);
+    %view(180+45,30)
+    %M(t)= getframe;
+end
+
+%movie(M);
+%movie2avi(M,'transport2.avi','quality',100)
+Uex = exactTransport(tend,J);
+Uerror = U - Uex;
+surf(x,y,U); axis([0 1 0 1 -1 1 -1 1]);
+maxVal = max(max(abs(Uerror)))
